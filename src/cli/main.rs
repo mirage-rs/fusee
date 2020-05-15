971a3381ca2e852b5e9004246e2001de6ec1903b
Justus K <justus.k@protonmail.com>
2020-05-15 19:40:04 +0200

    Add log crate and init pretty_env_logger

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,3 +1,12 @@
+use log::*;
+use pretty_env_logger::formatted_builder;
+
 fn main() {
-    println!("Hello, world!");
+    let mut logger = formatted_builder();
+    logger.filter(None, LevelFilter::Debug);
+    logger.init();
+
+    info!("such information");
+    warn!("o_O");
+    error!("much error");
 }
